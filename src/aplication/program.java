4c6c9d25fe3a05d522f2bc0b70c1ee00e6990323
Paulo sales <paulounopar@hotmail.com>
2019-02-17 15:02:50 -0300

    Seller entity class

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -1,12 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.seller;
 
 public class program {
 
 	public static void main(String[] args) {
 	Department obj = new Department(1,"Logistica");
+	seller seller = new seller(2, "paulo","sales@bol",new Date(),4.000,obj);
 	System.out.println(obj);
+	System.out.println(seller);
 
 	}
 
